c826976cace2d3039ff7bab549edceaa872a52e7
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2020-07-02 21:04:29 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        
+        n = s.nextInt();
+        if(n % 2 == 0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+	}
+    }
+}
