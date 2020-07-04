065a2381f37deec9f2bd7d92833f9af0ab393611
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2020-07-04 19:46:02 +0530

    Create print_nos_rev_skip2nos.java

---
+++ print_nos_rev_skip2nos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int number, i;
+		sc = new Scanner(System.in);
+		
+		
+		number = sc.nextInt();	
+		
+		for(i = number; i >= 0; i-=3)
+		{
+		    
+			System.out.print(i +"\t"); 
+		}	
+	}
+}
